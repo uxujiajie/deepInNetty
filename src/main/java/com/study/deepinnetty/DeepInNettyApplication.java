f5fac698c1599c9ba764898f6902071ecd9be8a6
xu1 <424255910@qq.com>
2021-11-13 15:06:45 +0800

    init file

---
+++ src/main/java/com/study/deepinnetty/DeepInNettyApplication.java
@@ -0,0 +1,13 @@
+package com.study.deepinnetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepInNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepInNettyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/deepinnetty/DeepInNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.deepinnetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepInNettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
